da46c2513cc9c22f643ff9eb63c0f3e2d589e059
SrinivasPabballa <srinivassunny64@gmail.com>
2025-07-17 19:01:06 +0530

    Test

---
+++ 01-test.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+
+Person1=$1
+Person2=$2
+
+
+echo "$Person1 :: Hi $Person2 ! How are you?"
+echo "$Person2 :: Hi, Iam good. How about you."
+echo "$Person1 :: Yeah fine. How's your work?"
+echo "$Person2 :: Going well, THinking to updgrade to Devops"
